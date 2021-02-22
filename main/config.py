809fb62a06bc4781c249174db88dbd7615e2fc7b
Julio Suriano <jjsuriano@gmail.com>
2021-02-22 05:55:55 -0600

    initial project setup

---
+++ main/config.py
@@ -0,0 +1,5 @@
+import os
+
+
+class Config(object):
+    SECRET_KY = os.environ.get('SECRET_KEY') or "w3mrXYW--ULt6t%x^$tDeyf77tw&bM=G?ArjPNDHkr#d%Sb68aCH2d?7-ZxqJq%rQhJ_mkkA-fn%cZzj?af2Nzx@%AU^ZAkGxDn9JU5s5q=WFjM66Xcc-3GQ!sxk&ajSJK3fP93-UfS=whMSqmcT@CfpRv!YCDY937dxZ34nxvY?!U*4SvU3D7Ku$!xsP&4YRdBRDjevWDyp7gwBT6ej%L9tQJs8L@y5VT5aSAJ4-qAjaPCYP%ddb?PeuHva=MRR"
